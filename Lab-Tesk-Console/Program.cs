2a9c6633223a807a561190a68092d7b8a14394f8
Earl Platt III <36335481+eaplatt3@users.noreply.github.com>
2019-11-25 09:49:52 -0500

    Init Commit

---
+++ Lab-Tesk-Console/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Tesk_Console
+{
+    class Program
+    {
+        static void Message()
+        {
+            Console.WriteLine("I am The Worker Thread");
+        }
+
+
+        static void Main(string[] args)
+        {
+            Action a = new Action(Message);
+
+            Task t = new Task(a);
+
+            t.Start();
+            Console.WriteLine("Main Thread Done");
+        }
+    }
+}
